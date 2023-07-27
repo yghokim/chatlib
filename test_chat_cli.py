89245c627f8f93457b7ea6169fa02a14d81ebba0
Young-Ho Kim <yghokim@gmail.com>
2023-07-27 11:42:01 +0900

    Updated aiohttp to resolve vulnerability. Added test_chat_cli.py

---
+++ test_chat_cli.py
@@ -0,0 +1,18 @@
+import asyncio
+from os import path, getcwd, getenv
+
+import openai
+from dotenv import load_dotenv
+
+from chatlib import cli
+from chatlib.chatbot.generators import ChatGPTResponseGenerator
+
+if __name__ == "__main__":
+    # Init OpenAI API
+    load_dotenv(path.join(getcwd(), ".env"))
+    openai.api_key = getenv('OPENAI_API_KEY')
+
+    asyncio.run(cli.run_chat_loop(ChatGPTResponseGenerator(
+        base_instruction="You are a helpful assistant that asks the user about their daily activity and feelings.",
+        initial_user_message="Hi!"
+    )))
