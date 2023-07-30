c36fac2c80c1bf6ea9fd91ef4b0e82ec83157c2c
Young-Ho Kim <yghokim@gmail.com>
2023-07-30 15:00:11 +0900

    Revised dialogue_utils.

--- chatlib/dialogue_utils.py
+++ chatlib/dialogue_utils.py
@@ -3,18 +3,18 @@ from typing import Callable
 from chatlib.chatbot import Dialogue, DialogueTurn
 
 
-def find_first_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn, int] | None:
+def find_first_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn|None, int]:
     for i in range(len(dialogue)):
         if test(dialogue[i]):
             return dialogue[i], i
 
-    return None
+    return None, -1
 
 
-def find_last_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn, int] | None:
+def find_last_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn|None, int]:
     for i in range(len(dialogue)-1, -1, -1):
         if test(dialogue[i]):
             return dialogue[i], i
 
-    return None
+    return None, -1
 
