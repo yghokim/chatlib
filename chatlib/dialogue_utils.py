82665814f929d4b9057c88939b28fded0bd5bc84
Young-Ho Kim <yghokim@gmail.com>
2023-07-31 22:12:40 +0900

    Added extract_last_turn_sequence

--- chatlib/dialogue_utils.py
+++ chatlib/dialogue_utils.py
@@ -3,7 +3,7 @@ from typing import Callable
 from chatlib.chatbot import Dialogue, DialogueTurn
 
 
-def find_first_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn|None, int]:
+def find_first_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn | None, int]:
     for i in range(len(dialogue)):
         if test(dialogue[i]):
             return dialogue[i], i
@@ -11,10 +11,20 @@ def find_first_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) ->
     return None, -1
 
 
-def find_last_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn|None, int]:
-    for i in range(len(dialogue)-1, -1, -1):
+def find_last_turn(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> tuple[DialogueTurn | None, int]:
+    for i in range(len(dialogue) - 1, -1, -1):
         if test(dialogue[i]):
             return dialogue[i], i
 
     return None, -1
 
+
+def extract_last_turn_sequence(dialogue: Dialogue, test: Callable[[DialogueTurn], bool]) -> Dialogue:
+    seq = []
+    for turn in reversed(dialogue):
+        if test(turn):
+            seq.append(turn)
+        else:
+            break
+
+    return seq
