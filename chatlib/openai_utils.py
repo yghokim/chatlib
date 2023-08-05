22c40834210ac0be9f2e6983498428d5c2fff86b
Young-Ho Kim <yghokim@gmail.com>
2023-08-05 22:47:58 +0900

    Fixed wrong gpt model name.

--- chatlib/openai_utils.py
+++ chatlib/openai_utils.py
@@ -7,9 +7,9 @@ import openai
 
 class ChatGPTModel(StrEnum):
     GPT_3_5_latest = "gpt-3.5-turbo"
-    GPT_3_5_16k_latest = "gpt-3.1-turbo-16k"
+    GPT_3_5_16k_latest = "gpt-3.5-turbo-16k"
     GPT_4_latest = "gpt-4"
-    GPT_4_16k_latest = "gpt-4-16k"
+    GPT_4_32k_latest = "gpt-4-32k"
     GPT_4_0613 = "gpt-4-0613"
     GPT_3_5_0613 = "gpt-3.5-0613"
 
