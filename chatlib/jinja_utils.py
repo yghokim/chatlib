f2edd44b354af139d1602978f1a7fa09d40b3065
Young-Ho Kim <yghokim@younghokim.net>
2023-08-08 14:22:04 +0900

    Added custom conjunction filter to jinja environment.

--- chatlib/jinja_utils.py
+++ chatlib/jinja_utils.py
@@ -1,13 +1,21 @@
 from jinja2 import Environment, Template, Undefined
 
-__jinja_env = None
+def list_with_conjunction(value, conjunction='and'):
+    if len(value) == 1:
+        return value[0]
+    elif len(value) == 2:
+        return f'{value[0]} {conjunction} {value[1]}'
+    else:
+        return ', '.join(value[:-1]) + f', {conjunction} {value[-1]}'
 
+__jinja_env = None
 
 def __get_jinja_env() -> Environment:
     global __jinja_env
 
     if __jinja_env is None:
         __jinja_env = Environment(undefined=Undefined)
+        __jinja_env.filters["list_with_conjunction"] = list_with_conjunction
     return __jinja_env
 
 
