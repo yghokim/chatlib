306a85e580cf13a73acfa152972084c99dd66990
Young-Ho Kim <yghokim@younghokim.net>
2024-01-30 13:48:31 +0900

    Refactored model generators.

--- chatlib/chatbot/generators/llama.py
+++ chatlib/chatbot/generators/llama.py
@@ -3,24 +3,25 @@ from typing import Callable, Awaitable, Any
 
 from jinja2 import Template
 
-from chatlib.chat_completion import ChatCompletionMessage
-from chatlib.chatbot import TokenLimitExceedHandler, ChatCompletionResponseGenerator
-from chatlib.openai_utils import GPTChatCompletionAPI, ChatGPTModel
+from chatlib.azure_llama2_utils import AzureLlama2ChatCompletionAPI
+from chatlib.chat_completion import ChatCompletionAPI, ChatCompletionMessage
+from chatlib.chatbot import ChatCompletionResponseGenerator, TokenLimitExceedHandler
 
 
-class ChatGPTResponseGenerator(ChatCompletionResponseGenerator):
+class Llama2ResponseGenerator(ChatCompletionResponseGenerator):
+
     @classmethod
     @cache
-    def get_api(cls) -> GPTChatCompletionAPI:
-        return GPTChatCompletionAPI()
+    def get_api(cls) -> AzureLlama2ChatCompletionAPI:
+        return AzureLlama2ChatCompletionAPI()
 
-    def __init__(self, model: str = ChatGPTModel.GPT_4_0613, base_instruction: str | Template | None = None,
+    def __init__(self, base_instruction: str | Template | None = None,
                  instruction_parameters: dict | None = None,
                  initial_user_message: str | list[ChatCompletionMessage] | None = None,
                  chat_completion_params: dict | None = None,
                  function_handler: Callable[[str, dict | None], Awaitable[Any]] | None = None,
                  special_tokens: list[tuple[str, str, Any]] | None = None, verbose: bool = False,
                  token_limit_exceed_handler: TokenLimitExceedHandler | None = None, token_limit_tolerance: int = 1024):
-        super().__init__(self.get_api(), model, base_instruction, instruction_parameters, initial_user_message,
+        super().__init__(self.get_api(), "Llama-2-70b-chat-hf", base_instruction, instruction_parameters, initial_user_message,
                          chat_completion_params, function_handler, special_tokens, verbose, token_limit_exceed_handler,
                          token_limit_tolerance)
