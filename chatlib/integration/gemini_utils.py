fde5877066ddd6d34c8c387eef5a8b7bc2dc4aa6
Young-Ho Kim <yghokim@gmail.com>
2024-02-01 18:42:59 +0900

    Refactored Gemini API.

--- chatlib/integration/gemini_utils.py
+++ chatlib/integration/gemini_utils.py
@@ -13,26 +13,38 @@ import google.generativeai as genai
 
 from chatlib.chatbot import RegenerateRequestException
 
-
 # https://ai.google.dev/tutorials/python_quickstart
 # https://github.com/google/generative-ai-python/blob/main/google/generativeai/generative_models.py#L382-L423
 
 _SAFETY_SETTINGS_BLOCK_NONE = [
     {
-        "category" : category,
+        "category": category,
         "threshold": "BLOCK_NONE"
-    } for category in ["HARM_CATEGORY_SEXUALLY_EXPLICIT", "HARM_CATEGORY_HATE_SPEECH", "HARM_CATEGORY_HARASSMENT", "HARM_CATEGORY_DANGEROUS_CONTENT"]
+    } for category in ["HARM_CATEGORY_SEXUALLY_EXPLICIT", "HARM_CATEGORY_HATE_SPEECH", "HARM_CATEGORY_HARASSMENT",
+                       "HARM_CATEGORY_DANGEROUS_CONTENT"]
 ]
+
+GEMINI_PRO_TOKEN_LIMIT = 30720
+
 
 class GeminiChatMessageRole(StrEnum):
     User = "user"
     Model = "model"
 
+    @classmethod
+    def from_chat_completion_role(cls, role: ChatCompletionMessageRole) -> 'GeminiChatMessageRole':
+        if role == ChatCompletionMessageRole.SYSTEM:
+            return cls.User
+        elif role == ChatCompletionMessageRole.ASSISTANT:
+            return cls.Model
+        elif role == ChatCompletionMessageRole.USER:
+            return cls.User
+
 
 def convert_to_gemini_message(message: ChatCompletionMessage) -> dict:
     return dict(
         parts=[message.content],
-        role=GeminiChatMessageRole.User if message.role is ChatCompletionMessageRole.USER or message.role is ChatCompletionMessageRole.SYSTEM else GeminiChatMessageRole.Model)
+        role=GeminiChatMessageRole.from_chat_completion_role(message.role))
 
 
 def convert_to_gemini_messages(messages: list[ChatCompletionMessage]) -> list[dict]:
@@ -76,7 +88,7 @@ class GeminiAPI(ChatCompletionAPI):
     def is_messages_within_token_limit(self, messages: list[ChatCompletionMessage], model: str,
                                        tolerance: int = 120) -> bool:
         self._assert_authorize()
-        return self.count_token_in_messages(messages, model) < 4096 - tolerance
+        return self.count_token_in_messages(messages, model) < GEMINI_PRO_TOKEN_LIMIT - tolerance
 
     def __convert_messages(self, messages: list[ChatCompletionMessage]) -> list[ChatCompletionMessage]:
         # Tweak system instruction
@@ -87,19 +99,22 @@ class GeminiAPI(ChatCompletionAPI):
             )
 
         if len(messages) >= 2:
-            if messages[0].role == ChatCompletionMessageRole.SYSTEM and messages[1].role == ChatCompletionMessageRole.ASSISTANT:
+            if messages[0].role == ChatCompletionMessageRole.SYSTEM and messages[1].role is ChatCompletionMessageRole.ASSISTANT:
                 return messages
             else:
-                return [messages[0]] + [ChatCompletionMessage(self.__injected_initial_system_message, ChatCompletionMessageRole.ASSISTANT)] + messages[1:]
+                return [messages[0]] + [ChatCompletionMessage(self.__injected_initial_system_message,
+                                                              ChatCompletionMessageRole.ASSISTANT)] + messages[1:]
         else:
-            return messages + [ChatCompletionMessage(self.__injected_initial_system_message, ChatCompletionMessageRole.ASSISTANT), ChatCompletionMessage("Hi!", ChatCompletionMessageRole.USER)]
+            return messages + [
+                ChatCompletionMessage(self.__injected_initial_system_message, ChatCompletionMessageRole.ASSISTANT),
+                ChatCompletionMessage("Hi!", ChatCompletionMessageRole.USER)]
 
     async def _run_chat_completion_impl(self, model: str, messages: list[ChatCompletionMessage], params: dict,
                                         trial_count: int = 5) -> Any:
         injected_messages = self.__convert_messages(messages)
 
         converted_messages = convert_to_gemini_messages(injected_messages)
-        response: GenerateContentResponse = self.model().generate_content(
+        response: GenerateContentResponse = await self.model().generate_content_async(
             contents=converted_messages,
             generation_config=params,
             safety_settings=self.__safety_settings
@@ -108,7 +123,8 @@ class GeminiAPI(ChatCompletionAPI):
         converted_choices = [convert_candidate_to_choice(candidate) for candidate in response.candidates]
 
         return {"choices": converted_choices,
-                "usage": {}, "model": "gemini pro", "raw_response_metadata": {"safety_ratings": {r.category:r.probability for r in response.prompt_feedback.safety_ratings}}}
+                "usage": {}, "model": "gemini pro", "raw_response_metadata": {
+                "safety_ratings": {r.category: r.probability for r in response.prompt_feedback.safety_ratings}}}
 
     def count_token_in_messages(self, messages: list[ChatCompletionMessage], model: str) -> int:
         self._assert_authorize()
