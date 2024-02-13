db6a6303d0633e0839af4d028b6f8bbc6e259618
Young-Ho Kim <yghokim@gmail.com>
2024-02-13 23:18:46 +0900

    Stylize AI message.

--- chatlib/cli.py
+++ chatlib/cli.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from os import getcwd, path
 from typing import Callable, TypeAlias, Awaitable
 
@@ -10,15 +11,17 @@ from yaspin import yaspin
 from chatlib.chat_completion_api import make_non_empty_string_validator
 from chatlib.chatbot import ResponseGenerator, TurnTakingChatSession, DialogueTurn, MultiAgentChatSession
 
+from prompt_toolkit import print_formatted_text, HTML
+import html
+
 CommandDef: TypeAlias = tuple[list[str] | str, Callable[[TurnTakingChatSession], None | Awaitable]]
 
 CLEAR_LINE = '\033[K'
 
-
 async def regen(session: TurnTakingChatSession):
     system_turn = await session.regenerate_last_system_message()
     if system_turn is not None:
-        print(__turn_to_string(system_turn))
+        __print_turn(system_turn)
 
 
 DEFAULT_TEST_COMMANDS = [
@@ -33,6 +36,16 @@ def __turn_to_string(turn: DialogueTurn) -> str:
         return f"<AI> {turn.message} ({turn.metadata.__str__() if turn.metadata is not None else None}) - {turn.processing_time} sec"
 
 
+def __print_turn(turn: DialogueTurn, user_alias="You"):
+
+    if turn.is_user:
+        text = f"<b>{html.escape(f'<{user_alias}>')}</b> {html.escape(turn.message)}"
+    else:
+        text = f"<b>{html.escape('<AI>')}</b> <i><yellowgreen>{html.escape(turn.message)}</yellowgreen></i>\n{html.escape(json.dumps(turn.metadata, indent=2)) if turn.metadata is not None else None} - {turn.processing_time} sec"
+
+    print_formatted_text(HTML(text))
+
+
 async def run_chat_loop(response_generator: ResponseGenerator, commands: list[CommandDef] | None = None):
     session_id = generate_id()
 
@@ -44,7 +57,8 @@ async def run_chat_loop(response_generator: ResponseGenerator, commands: list[Co
 
 async def run_chat_loop_from_session(session: TurnTakingChatSession, initialize: bool = False,
                                      commands: list[CommandDef] | None = None):
-    print(f"Resume chat for session {session.id}")
+    if not initialize:
+        print(f"Resume chat for session {session.id}")
 
     spinner = yaspin(text="Thinking...")
 
@@ -52,16 +66,17 @@ async def run_chat_loop_from_session(session: TurnTakingChatSession, initialize:
         spinner.start()
         system_turn = await session.initialize()
         spinner.stop()
-        questionary.print(__turn_to_string(system_turn))  # Print initial message
+
+        __print_turn(system_turn)  # Print initial message
     elif len(session.dialog) > 0:
         for turn in session.dialog:
-            questionary.print(__turn_to_string(turn))
+            __print_turn(turn)
 
         questionary.print("========Continue chat==========")
 
     while True:
-        user_message = await questionary.text("<You>", validate=make_non_empty_string_validator("A message should not be empty.")).ask_async()
-
+        user_message = await questionary.text("<You>", validate=make_non_empty_string_validator(
+            "A message should not be empty."), qmark="*").ask_async()
 
         matched_command_actions = [action for cmd, action in commands if (user_message.lower() == cmd if isinstance(cmd,
                                                                                                                     str) else user_message.lower() in cmd)] if commands is not None else []
@@ -78,7 +93,7 @@ async def run_chat_loop_from_session(session: TurnTakingChatSession, initialize:
             spinner.start()
             system_turn = await session.push_user_message(DialogueTurn(user_message, is_user=True))
             spinner.stop()
-            questionary.print(__turn_to_string(system_turn))
+            __print_turn(system_turn)
 
 
 async def run_auto_chat_loop(agent_generator: ResponseGenerator, user_generator: ResponseGenerator,
@@ -88,7 +103,7 @@ async def run_auto_chat_loop(agent_generator: ResponseGenerator, user_generator:
 
     print(f"Start a chat session (id: {session_id}).")
     session = MultiAgentChatSession(session_id, agent_generator, user_generator)
-    dialogue = await session.generate_conversation(max_turns, lambda turn: print(__turn_to_string(turn)))
+    dialogue = await session.generate_conversation(max_turns, lambda turn: __print_turn(turn, user_alias="User"))
 
     output_path = output_path or path.join(getcwd(), f"auto_chat_{session_id}.txt")
     with open(output_path, "w", encoding='utf-8') as f:
