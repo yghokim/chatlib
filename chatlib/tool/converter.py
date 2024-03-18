91b2d857adaeea1e52c0e78d23de800e758daa94
Young-Ho Kim <yghokim@younghokim.net>
2024-03-18 15:50:58 +0900

    Added type converter for mappers.

--- chatlib/tool/converter.py
+++ chatlib/tool/converter.py
@@ -1,16 +1,30 @@
 import json
 import re
+from functools import cache
 from typing import Type, Callable, Any, TypeVar
 
-from pydantic import BaseModel
+from pydantic import BaseModel, TypeAdapter
+
+DataType = TypeVar('DataType')
 
 BaseModelType = TypeVar('BaseModelType', bound=BaseModel)
+
+
+@cache
+def get_type_adapter(cls: Type[DataType]) -> TypeAdapter[DataType]:
+    return TypeAdapter[cls]
 
 
 def generate_pydantic_converter(cls: Type[BaseModelType]) -> tuple[
     Callable[[str, Any], BaseModelType], Callable[[BaseModelType, Any], str]]:
     return (lambda input, params: cls(**str_to_json_dict_converter(input, params))), (
         lambda input, params: input.json())
+
+
+def generate_type_converter(cls: Type[DataType]) -> tuple[
+    Callable[[str, Any], DataType], Callable[[DataType, Any], str]]:
+    return (lambda input, params: get_type_adapter(cls).validate_json(input)), (
+        lambda input, params: input.dump_json())
 
 
 markdown_json_block_pattern = r'^```(json)?\s*(.*?)\s*```$'
